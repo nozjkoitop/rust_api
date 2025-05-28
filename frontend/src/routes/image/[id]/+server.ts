51c8121974cebbab4eed4d748ba36dedd48b1320
nozjkoitop <mikhailsviatohorof@gmail.com>
2025-05-28 21:51:11 +0200

    fix 401

--- frontend/src/routes/image/[id]/+server.ts
+++ frontend/src/routes/image/[id]/+server.ts
@@ -1,16 +1,9 @@
 import { API_URL } from '$lib';
 import type { RequestHandler } from './$types';
 
-export const GET: RequestHandler = async ({ cookies, params, fetch }) => {
-  const token = cookies.get('jwt');
-  if (!token) {
-    return new Response('Unauthorized', { status: 401 });
-  }
+export const GET: RequestHandler = async ({ params, fetch }) => {
 
   const upstream = await fetch(`${API_URL}/image/${params.id}`, {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
   });
 
   if (!upstream.ok) {
