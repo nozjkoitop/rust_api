4e80978cb4cfc0c9075735399e7ee31ead8041c4
nozjkoitop <mikhailsviatohorof@gmail.com>
2025-05-28 21:51:11 +0200

    fix 401

--- frontend/src/routes/image/[id]/+server.ts
+++ frontend/src/routes/image/[id]/+server.ts
@@ -3,9 +3,6 @@ import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ cookies, params, fetch }) => {
   const token = cookies.get('jwt');
-  if (!token) {
-    return new Response('Unauthorized', { status: 401 });
-  }
 
   const upstream = await fetch(`${API_URL}/image/${params.id}`, {
     headers: {
